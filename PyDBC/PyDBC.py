94966a9541f25c272c6a160a356b3e5411cb4ec2
mxf <maxuefeng@lawcubes.com>
2019-07-25 23:28:47 +0800

    Version 2

--- PyDBC/PyDBC.py
+++ PyDBC/PyDBC.py
@@ -3,15 +3,20 @@
 
 import pymysql
 import sys
+from tqdm import tqdm
+import threading
+from concurrent.futures import ThreadPoolExecutor, wait
+from queue import Queue
+import time
 
 """
 ** Python Database Connectivity
 
  * @author Ma Xuefeng
 
- * @date 2018/7/14
+ * @date 2018/11/19
 
- * @version v1.0.0
+ * @version v2.0.0
 """
 class PyDBC:
     """
@@ -20,13 +25,17 @@ class PyDBC:
 
     # Configer
     host = 'localhost'
-    user = 'user'
+    user = 'user_name'
     password = 'password'
     db = 'database_name'
 
     # is_debug : True  - print(sql)
     #            False - silence
-    is_debug = True
+    is_debug = False
+    save_many_batch = 10000 # 0 means unlimit
+    thread_amount = 10
+
+    pool_amount = 5
     # Other Parameter
     connection = None
 
@@ -36,9 +45,7 @@ class PyDBC:
         self.connection = pymysql.connect(host = self.host,
                                          user = self.user,
                                          password = self.password,
-                                         db = self.db,
-                                         use_unicode=True,
-                                         charset='utf8')
+                                         db = self.db)
 
     def close(self):
         """ Close """
@@ -73,7 +80,7 @@ class PyDBC:
         :return: rows
         """
         sql = 'select '
-        if columns != None and len(columns)>0:
+        if columns != None and len(columns) > 0:
             for index, column in enumerate(columns):
                 if index > 0:
                     sql += ' , '
@@ -86,7 +93,7 @@ class PyDBC:
             for index, (key, value) in enumerate(conditions.items()):
                 if index > 0:
                     sql += ' && '
-                sql += key+" = '"+conditions[key]+"'"
+                sql += "{} = '{}'".format(key, conditions[key])
         if self.is_debug:
             print('SQL in [PyDBC.get_all] : ' + sql)
         results = self.execute(sql, False)
@@ -125,8 +132,8 @@ class PyDBC:
                 key += ' , '
                 value += ' , '
             key += key_
-            if len(value_)>0:
-                value += "'" + ''.join(value_) + "'"
+            if len('{}'.format(value_))>0:
+                value += "'{}'".format(value_)
             else:
                 value += "''"
         key += ' ) '
@@ -134,6 +141,141 @@ class PyDBC:
         sql = 'insert into ' + table + key + ' value ' + value
         if self.is_debug:
             print('SQL in [PyDBC.save] : ' + sql)
+        row_count = self.execute(sql, True)
+        if row_count < 1:
+            sys.stderr.write('Fail in saving : SQL in [DB.save] ==> ' + sql + '\n')
+        return row_count
+
+
+    def save_many_worker(self, table, row, values_batch):
+        pyDBC = self.connector_queue.get()
+        pyDBC.save_many(table, row, values_batch)
+        self.connector_queue.put(pyDBC)
+
+
+    def _save_many_by_batch(self, table, row, values):
+        """
+        Insert Many Data into Table (Private)
+        :warning row & value must be the same order
+        :param table: table name ( type : string)
+        :param rows: row name ( type : list )
+        :param values: values ( type : list of list )
+        :return: row_count affected
+        """
+        executor = ThreadPoolExecutor(max_workers=self.thread_amount)
+        tasks = []
+        values_batches = []
+        if self.save_many_batch > 0:
+            loops = len(values) // self.save_many_batch
+            print('Preparing loops...')
+            for index in tqdm(range(loops+1)):
+                if index < loops:
+                    values_batches.append(values[index * self.save_many_batch: (index+1) * self.save_many_batch])
+                else:
+                    values_batches.append(values[index * self.save_many_batch:])
+        else:
+            values_batches.append(values[:])
+
+        print('Saving...')
+        threads = []
+        for values_batch in values_batches:
+            tasks.append(executor.submit(self.save_many_worker, table, row, values_batch,))
+        #     t = threading.Thread(target=self.save_many_worker, args=(table, row, values_batch,))
+        #     t.start()
+        #     threads.append(t)
+        #     if len(threads) > (self.thread_amount - 1 if self.thread_amount > 0 else 0):
+        #         for t in threads:
+        #             t.join()
+        #         threads = []
+        # for t in threads:
+        #     t.join()
+        for task in tqdm(tasks):
+            while not task.done():
+                time.sleep(0.01)
+        # wait(tasks)
+
+
+    def save_many_by_batch(self, table, row, values, **kwargs):
+        """
+        Insert Many Data into Table
+        :warning row & value must be the same order
+        :param table: table name ( type : string)
+        :param rows: row name ( type : list )
+        :param values: values ( type : list of list )
+        :return: row_count affected
+        """
+        if 'save_many_batch' in kwargs.keys():
+            self.save_many_batch = kwargs['save_many_batch']
+        if 'thread_amount' in kwargs.keys():
+            self.thread_amount = kwargs['thread_amount']
+
+        self.init_pool()
+        self._save_many_by_batch(table, row, values)
+        self.close_pool()
+
+
+    def init_pool(self):
+        """
+        初始化连接池
+        :return: 
+        """
+        self.connector_queue = Queue()
+        self.connector_queue.put(self)
+        while self.connector_queue.qsize() < self.pool_amount:
+            self.connector_queue.put(PyDBC())
+
+
+    def close_pool(self):
+        """
+        关闭连接池
+        :return: 
+        """
+        while not self.connector_queue.empty():
+            pyDBC = self.connector_queue.get()
+            if pyDBC != self:
+                pyDBC.close()
+
+
+    def save_many(self, table, row, values):
+        """
+        Insert Many Data into Table
+        :warning row & value must be the same order
+        :param table: table name ( type : string)
+        :param rows: row name ( type : list )
+        :param values: values ( type : list )
+        :return: row_count affected
+        """
+        if self.is_debug:
+            print('Preparing Keys...')
+        key =' ( '
+        def add_keys(index, key, key_):
+            if index > 0:
+                key += ' , '
+            key += key_
+            return key
+        for index, key_ in enumerate(row):
+            key = add_keys(index, key, key_)
+        key += ' ) '
+
+        if self.is_debug:
+            print('Prepare Values...')
+        value = ''
+        def add_values(index, value, value_row):
+            if index > 0:
+                value += ','
+            value += '('
+            for i, value_ in enumerate(value_row):
+                if i > 0:
+                    value += ','
+                value += "'{}'".format(value_) if value_ != None else "{}".format('NULL')
+            value += ')'
+            return value
+        for index, value_row in enumerate(values):
+            value = add_values(index, value, value_row)
+
+        sql = 'insert into {} {} values {} '.format(table, key, value)
+        if self.is_debug:
+            print('SQL in [PyDBC.save_many] : ' + sql)
         row_count = self.execute(sql, True)
         if row_count < 1:
             sys.stderr.write('Fail in saving : SQL in [DB.save] ==> ' + sql + '\n')
@@ -153,11 +295,11 @@ class PyDBC:
         for index,( key, value) in enumerate(columns.items()):
             if index > 0 :
                 set_sql += ' , '
-            set_sql += key + " = '" + value + "' "
+            set_sql += " {} = '{}'".format(key, value)
         for index, (key, value) in enumerate(conditions.items()):
             if index > 0:
                 conditions_sql += ' && '
-            conditions_sql += key + " = '" + value + "' "
+            conditions_sql += " {} = '{}'".format(key, value)
         sql = 'update ' + table + ' set ' + set_sql + ' where ' + conditions_sql
         if self.is_debug:
             print(sql)
@@ -178,10 +320,15 @@ class PyDBC:
         for index,( key, value) in enumerate(conditions.items()):
             if index > 0:
                 sql += ' && '
-            sql += key + " = '" + value + "'"
+            sql += "{} = '{}'".format(key, value)
         if self.is_debug:
             print(sql)
         row_count = self.execute(sql, True)
         if row_count < 1:
             sys.stderr.write('Fail in updating : SQL in [PyDBC.save] ==> ' + sql + '\n')
         return row_count
+        
+    def query_close(self, table, columns=None, conditions=None):
+        datalist = self.get_all(table=table, columns=columns, conditions=conditions)
+        self.close()
+        return datalist
